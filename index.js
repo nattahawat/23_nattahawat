33e3f7ea3886eba78b94610ca4c4b0d1edc6c3a6
nattahawat <151804831+nattahawat@users.noreply.github.com>
2025-02-28 13:39:55 +0700

    Update index.js
    - fix mysql

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mysql = require('mysql/promise');
+const mysql = require('mysql2/promise');
 const app = express();
 
 app.use(cors());
